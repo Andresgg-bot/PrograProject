94e3fa0690e7c925f3079523b3038ac1ebc84f81
Andres Chinchilla <achinchilla90184@ufide.ac.cr>
2022-08-01 20:36:15 -0600

    Added ContactEmail

---
+++ Proyecto_Progra_MVC/Components/Cartero.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Proyecto_Progra_MVC.Contracts;
+using Proyecto_Progra_MVC.Domain.Models.ConfigurationModels;
+using Proyecto_Progra_MVC.Domain.Models.PlainModels;
+using System.Net;
+using System.Net.Mail;
+
+namespace Proyecto_Progra_MVC.Components
+{
+    public class Cartero : ICartero
+    {
+        public Cartero(IOptions<ConfiguracionSmtp> configuracion)
+        {
+            Configuracion = configuracion.Value;
+        }
+
+        ConfiguracionSmtp Configuracion;
+
+        public void Enviar(CorreoElectronico correo)
+        {
+            var cliente =
+                new SmtpClient
+                {
+                    Host = Configuracion.Servidor,
+                    Port = Configuracion.Puerto,
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials =
+                        new NetworkCredential(Configuracion.Usuario, Configuracion.Contrasena)
+                };
+
+            var mensaje =
+                new MailMessage
+                {
+                    From = new MailAddress(Configuracion.Remitente),
+                    Subject = correo.Asunto,
+                    Body = correo.Cuerpo,
+                    IsBodyHtml = false
+                };
+
+            mensaje.To.Add(new MailAddress(correo.Destinatario));
+            cliente.Send(mensaje);
+        }
+    }
+}
---
+++ Proyecto_Progra_MVC/Contracts/ICartero.cs
@@ -0,0 +1,10 @@
+﻿using Proyecto_Progra_MVC.Domain.Models.PlainModels;
+
+namespace Proyecto_Progra_MVC.Contracts
+{
+    public interface ICartero
+    {
+        void Enviar(CorreoElectronico correo);
+
+    }
+}
