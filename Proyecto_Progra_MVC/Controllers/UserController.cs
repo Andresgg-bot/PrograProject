a77a0370f88e6b6cc9ed5bd989bfce12fe3ac086
Frey <fbogantes00837@ufide.ac.cr>
2022-08-03 18:07:17 -0600

    userController

--- Proyecto_Progra_MVC/Controllers/UserController.cs
+++ Proyecto_Progra_MVC/Controllers/UserController.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-/*using Proyecto_Progra_MVC.DataAccess.Data;
-using Proyecto_Progra_MVC.DataAccess.Repository;
-using Proyecto_Progra_MVC.DataAccess.Repository.UnityOfWork;
-using Proyecto_Progra_MVC.Models.DataModels;*/
+using Proyecto_Progra_MVC.Infraestructure.Data;
+using Proyecto_Progra_MVC.Infraestructure.Repository;
+using Proyecto_Progra_MVC.Infraestructure.Repository.UnitOfWork;
+using Proyecto_Progra_MVC.Domain.Models.Entities;
 
 namespace Proyecto_Progra_MVC.Controllers
 {
     public class UserController : Controller
     {
 
-        /*public UserController(IUnitOfWork<ApplicationDbContext> unitOfWork)
+        public UserController(IUnitOfWork<ApplicationDbContext> unitOfWork)
         {
             UnitOfWork = unitOfWork;
             Repository = UnitOfWork.Repository<User>();
@@ -18,19 +18,19 @@ namespace Proyecto_Progra_MVC.Controllers
         readonly IUnitOfWork<ApplicationDbContext> UnitOfWork;
         readonly IRepository<User> Repository;
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         public IActionResult Profile(string id)
         {
-            User user = Repository.Get(filter: usuario => usuario.Email == id);
+            User user = Repository.Obtener(usuario => usuario.Email == id);
             if (user == null)
             {
                 return NotFound();
             }
             return View(user);
-        }*/
+        }
     }
 }
