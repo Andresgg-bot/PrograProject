7ee11d54cbbb345a5f55d8776e274cccaf46ced4
PC-ANDRES\andre <andreslml.ab@gmail.com>
2022-08-13 20:33:32 -0600

    Change and create new entities

---
+++ Proyecto_Progra_MVC.Domain/Models/Entities/Info.cs
@@ -0,0 +1,33 @@
+﻿using Proyecto_Progra_MVC.Domain.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Progra_MVC.Domain.Models.Entities
+{
+    public class Info
+    {
+        [Key]
+        public int IdInfo { get; set; }
+
+        public float Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public int Age { get; set; }
+
+        [Display(Name = "Physical Activity")]
+        public PhysicalActivity PhysicalActivity { get; set; }
+
+        [Display(Name = "Gender")]
+        public Genders Gender { get; set; }
+
+        public DateTime InfoDate { get; set; }
+
+        public User User { get; set; }
+
+    }
+}
