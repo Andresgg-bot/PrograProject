ddb84c7c41d65e9843f8c09b33deb0ab1483a913
PC-ANDRES\andre <andreslml.ab@gmail.com>
2022-08-13 20:35:21 -0600

    Migrations

---
+++ Proyecto_Progra_MVC.Infraestructure/Migrations/20220809210714_Changes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Progra_MVC.Infraestructure.Migrations
+{
+    public partial class Changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    IdInfo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    InfoDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PhysicalActivity = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfo", x => x.IdInfo);
+                });
+        }
+    }
+}
